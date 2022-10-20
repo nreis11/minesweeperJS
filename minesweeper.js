74f6c4d1a6cdaa22d9de65f28975dc545fda0831
nreis11 <nicholasreis@icloud.com>
2022-10-20 11:47:04 -0700

    finish mvp

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,30 @@
 // Logic
 
-const TILE_STATUSES = {
+export const TILE_STATUSES = {
   HIDDEN: "hidden",
   MINE: "mine",
   NUMBER: "number",
   MARKED: "marked",
 };
+
+function nearbyTiles(board, tile) {
+  const adjTiles = [];
+  for (let row_delta = -1; row_delta <= 1; row_delta++) {
+    for (let col_delta = -1; col_delta <= 1; col_delta++) {
+      if (row_delta === 0 && col_delta === 0) {
+        continue;
+      }
+      const newRow = tile.row + row_delta;
+      const newCol = tile.col + col_delta;
+      const rowInBounds = newRow >= 0 && newRow < board.length;
+      const colInBounds = newCol >= 0 && newCol < board[tile.row].length;
+      if (rowInBounds && colInBounds) {
+        adjTiles.push(board[newRow][newCol]);
+      }
+    }
+  }
+  return adjTiles;
+}
 
 function getMinePositions(boardSize, numberOfMines) {
   const minePositions = new Set();
@@ -13,30 +32,64 @@ function getMinePositions(boardSize, numberOfMines) {
     const row = Math.floor(Math.random() * boardSize);
     const col = Math.floor(Math.random() * boardSize);
     const key = `${row},${col}`;
-    if (minePositions.has(key)) {
-      continue;
+    if (!minePositions.has(key)) {
+      minePositions.add(key);
     }
-    minePositions.add(key);
   }
-  return Array.from(minePositions);
+  return minePositions;
+}
+
+export function revealTile(board, tile) {
+  if (tile.status !== TILE_STATUSES.HIDDEN) {
+    return;
+  }
+
+  if (tile.mine) {
+    tile.status = TILE_STATUSES.MINE;
+    return;
+  }
+
+  tile.status = TILE_STATUSES.NUMBER;
+  const adjacentTiles = nearbyTiles(board, tile);
+  console.log(
+    "🚀 ~ file: minesweeper.js ~ line 54 ~ revealTile ~ adjacentTiles",
+    adjacentTiles
+  );
+  const mines = adjacentTiles.filter((tile) => tile.mine);
+  if (mines.length === 0) {
+    adjacentTiles.forEach(revealTile.bind(null, board));
+  } else {
+    tile.element.textContent = mines.length;
+  }
+}
+
+export function markTile(tile) {
+  if (
+    tile.status !== TILE_STATUSES.HIDDEN &&
+    tile.status !== TILE_STATUSES.MARKED
+  ) {
+    return;
+  }
+  if (tile.status === TILE_STATUSES.HIDDEN) {
+    tile.status = TILE_STATUSES.MARKED;
+  } else {
+    tile.status = TILE_STATUSES.HIDDEN;
+  }
 }
 
 export function createBoard(boardSize, numberOfMines) {
   const minePositions = getMinePositions(boardSize, numberOfMines);
-  console.log(
-    "🚀 ~ file: minesweeper.js ~ line 26 ~ createBoard ~ minePositions",
-    minePositions
-  );
   const board = [];
-  for (let i = 0; i < boardSize; i++) {
-    const row = [];
-    for (let j = 0; j < boardSize; j++) {
+  for (let row = 0; row < boardSize; row++) {
+    const currRow = [];
+    for (let col = 0; col < boardSize; col++) {
       const element = document.createElement("div");
       element.dataset.status = TILE_STATUSES.HIDDEN;
       const tile = {
-        row: i,
-        col: j,
+        row,
+        col,
         element,
+        mine: minePositions.has(`${row},${col}`),
         get status() {
           return this.element.dataset.status;
         },
@@ -44,9 +97,31 @@ export function createBoard(boardSize, numberOfMines) {
           this.element.dataset.status = value;
         },
       };
-      row.push(tile);
+      currRow.push(tile);
     }
-    board.push(row);
+    board.push(currRow);
   }
   return board;
+}
+
+export function checkWin(board, numMines) {
+  const hiddenTiles = board.reduce((count, row) => {
+    return (
+      count +
+      row.filter(
+        (tile) =>
+          tile.status === TILE_STATUSES.HIDDEN ||
+          tile.status === TILE_STATUSES.MARKED
+      ).length
+    );
+  }, 0);
+  return numMines === hiddenTiles;
+}
+
+export function checkLose(board) {
+  return board.some((row) => {
+    return row.some((tile) => {
+      return tile.status === TILE_STATUSES.MINE;
+    });
+  });
 }
--- script.js
+++ script.js
@@ -1,20 +1,76 @@
 // DiaplayUI
-import { createBoard } from "./minesweeper.js";
+import {
+  createBoard,
+  markTile,
+  revealTile,
+  TILE_STATUSES,
+  checkWin,
+  checkLose,
+} from "./minesweeper.js";
 
-const BOARD_SIZE = 2;
-const NUMBER_OF_MINES = 2;
+const BOARD_SIZE = 5;
+const NUMBER_OF_MINES = 1;
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
-console.log("🚀 ~ file: script.js ~ line 8 ~ board", board);
+
+const minesLeftText = document.querySelector("[data-mine-count");
 const boardElement = document.querySelector(".board");
+const messageText = document.querySelector(".subtext");
 
 board.forEach((row) => {
   row.forEach((tile) => {
     boardElement.append(tile.element);
+    tile.element.addEventListener("click", () => {
+      revealTile(board, tile);
+      checkGameEnd();
+    });
+    tile.element.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      markTile(tile);
+      listMinesLeft();
+    });
   });
 });
 
+function listMinesLeft() {
+  const markedTilesCount = board.reduce((count, row) => {
+    return (
+      count + row.filter((tile) => tile.status === TILE_STATUSES.MARKED).length
+    );
+  }, 0);
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
+}
+
+function checkGameEnd() {
+  const win = checkWin(board, NUMBER_OF_MINES);
+  const lose = checkLose(board);
+
+  if (win || lose) {
+    boardElement.addEventListener("click", stopProp, { capture: true });
+    boardElement.addEventListener("contextmenu", stopProp, { capture: true });
+  }
+
+  if (win) {
+    messageText.textContent = "You won!";
+  }
+  if (lose) {
+    messageText.textContent = "You lose!";
+    board.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile.status === TILE_STATUSES.MARKED) markTile(tile);
+        if (tile.mine) revealTile(board, tile);
+      });
+    });
+  }
+}
+
+function stopProp(e) {
+  e.stopImmediatePropagation();
+}
+
+minesLeftText.textContent = NUMBER_OF_MINES;
 boardElement.style.setProperty("--size", BOARD_SIZE);
+
 // createBoard (rows, cols, mines)
 // getNeighbors (board, row, col)
 // calculateMines (board, row, col)
